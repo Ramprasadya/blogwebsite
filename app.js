79ed676f5c862d6b919171ce003f45cf1dd1eeae
Ramprasadya <yadavramprasad563@gmail.com>
2023-01-23 08:14:19 +0530

    search the post using /posts/postname

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash')
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -46,6 +47,20 @@ app.post("/compose", (req, res) => {
  res.redirect("/")
 
 });
+
+app.get("/posts/:postname" ,(req,res)=>{
+  const requestedTitle = _.lowerCase(req.params.postname)
+  posts.map((post)=>{
+    const storedTitle = _.lowerCase(post.title)
+
+    if(storedTitle === requestedTitle ){
+      res.render("post",{
+        title : post.title,
+        desc : post.desc
+      })
+    }
+  })
+})
 
 app.listen(3000, function () {
   console.log(`Server started on port http://localhost:${3000}`);
